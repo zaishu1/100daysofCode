9096d28206f823a024715c93bcd8ffeba26e8fad
zai shu <zaishu@Ums-iPhone.lan>
2019-08-24 00:46:35 +0300

    day#3

--- 100daysofCode.playground/Contents.swift
+++ 100daysofCode.playground/Contents.swift
@@ -1,8 +1,8 @@
 import UIKit
 // day#1
 var str = "Hello, playground"
-//day#2
 
+//day#2
 let age: Int = 10
 var name = "Yousef"
 var eggPrice: Int = 15
@@ -10,4 +10,11 @@ var amountOfEggsBought: Int = 3
 var totalAmount = eggPrice * amountOfEggsBought
 print("subtotal: \(totalAmount)")
 
+
+// day#3
+let juicePrice = 5.0
+let chipsPrice = 10.0
+let VAT = 1.05
+var subtotal = (juicePrice + chipsPrice) * VAT
+print(subtotal)
 
