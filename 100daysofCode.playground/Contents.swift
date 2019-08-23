3443b91477c167c27eef9ee9416725063dc10bc7
zai shu <zaishu@Ums-iPhone.lan>
2019-08-24 00:34:53 +0300

    day#2

---
+++ 100daysofCode.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+// day#1
+var str = "Hello, playground"
+//day#2
+
+let age: Int = 10
+var name = "Yousef"
+var eggPrice: Int = 15
+var amountOfEggsBought: Int = 3
+var totalAmount = eggPrice * amountOfEggsBought
+print("subtotal: \(totalAmount)")
+
+
