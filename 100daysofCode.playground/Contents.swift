4e3e4526dcaf8cb5fcffe67f58c36c11e05addf4
zai shu <zaishu@Ums-iPhone.lan>
2019-08-24 01:10:40 +0300

    Week#1All

--- 100daysofCode.playground/Contents.swift
+++ 100daysofCode.playground/Contents.swift
@@ -18,3 +18,20 @@ let VAT = 1.05
 var subtotal = (juicePrice + chipsPrice) * VAT
 print(subtotal)
 
+var name1 = "Yousef"
+var name2 = "Yazeed"
+var both = name1 + " and " + name2
+
+name1 == name2 // false
+name1 != name2 // true
+name1 == "Yousef" // true
+name1 == "yousef" // false
+
+
+// day#4
+let expression1 = 90 < 100 //true
+let expression2 = 90 > 100 //false
+let compoundExpression1 = expression1 || expression2 //true
+let compoundExpression2 = expression1 && expression2 //false
+let compoundExpression3 = compoundExpression2 || expression2 //false
+let compoundExpression4 = expression1 && compoundExpression1 //true
---
+++ Quiz1 .playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+//Q1
+var salary = 5000
+var home = 1000
+var ess = 700
+var extra = 1300
+
+var totalS = 5000 * 12
+var spend = (home + ess + extra ) * 12
+var saved =  ( salary - (home + ess + extra ) ) * 12
+// -------------------------------------------------
+// Q2
+// String
+// int
+// float
+// bool
+// -------------------------------------------------
+//Q3
+// False
+// false
+// true 
+
+
+
